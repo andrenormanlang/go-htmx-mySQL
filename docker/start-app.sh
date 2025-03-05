4ab9e4de4307af08763a882214004a20ad86b2e1
andrenormanlang <andrenormanlang@gmail.com>
2025-03-05 13:41:50 +0100

    fixing docker for running github actions

--- docker/start-app.sh
+++ docker/start-app.sh
@@ -7,5 +7,13 @@ git config --global --add safe.directory '*'
 cd /cmsgo/migrations
 GOOSE_DRIVER="mysql" GOOSE_DBSTRING="root:root@tcp(mariadb:3306)/cms-and-go" goose up
 
-cd /gocms
-air -c ./docker/air.toml
+cd /cmsgo
+
+# Check if we're running in admin mode
+if [ "$1" = "--admin" ]; then
+  # Run admin app with admin-specific air config
+  air -c ./docker/air-admin.toml
+else
+  # Run regular app
+  air -c ./docker/air.toml
+fi
