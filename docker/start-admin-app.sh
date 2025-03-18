3cb080a1ed4804e13072ce6654a10f166896c917
andrenormanlang <andrenormanlang@gmail.com>
2025-03-19 00:38:59 +0100

    fixed makefile for docker admin app to run

--- docker/start-admin-app.sh
+++ docker/start-admin-app.sh
@@ -7,6 +7,8 @@ git config --global --add safe.directory '*'
 cd /gocms/migrations
 GOOSE_DRIVER="mysql" GOOSE_DBSTRING="root:root@tcp(mariadb:3306)/cms-and-go" goose up
 
+
 cd /gocms
 make build
+export SKIP_PREPARE_ENV=true
 air -c ./docker/air.admin.toml 
