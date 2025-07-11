2978dbc7a3f497a3670a71a048a8a16e963acb00
andre <andrenormanlang@gmail.com>
2025-07-11 17:59:28 +0200

    adding permalink support in the database

--- admin-app/app.go
+++ admin-app/app.go
@@ -23,6 +23,10 @@ func SetupRoutes(app_settings common.AppSettings, database database.Database) *g
 
 	r.POST("/pages", postPageHandler(database))
 
+	// ASSUME: permalink will not contain /
+	// POST gocms.com/permalinks/my/permalink
+	r.POST("/permalinks/:permalink/:post_id", postPermalinkHandler(database))
+
 	// For container health purposes
 	r.Any("/health", func(c *gin.Context) {
 		c.JSON(http.StatusOK, PostIdResponse{Id: 0})
